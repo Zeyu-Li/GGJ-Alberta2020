aa6effb2ea38cb6bacebdf982622ab46fe39e23b
goldentoaste <59712548+goldentoaste@users.noreply.github.com>
2020-01-31 21:56:19 -0700

    ZeroG code finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,14 +11,18 @@ public class PlayerController : MonoBehaviour
     public float jump;
     float moveVelocity;
 
-    bool zeroG = false;
+    public bool isZeroG = false;
 
     public float jetPackAccel = 0.3f;
     public float rotationSpd = 0.3f;
     public float maxGlideSpd = 6;
+    public float overPressure = 1.6f;
+    bool isRunning = false;
 
-    [Range(0f, 1f)]
-    public float brakeForce = 0.6f;
+    public Vector2 movementVec;
+    public Vector2 velocityVec;
+    [Range(1f, 2f)]
+    public float brakeForce = 1f;
 
     //Grounded Vars
     bool grounded = false;
@@ -26,7 +30,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (zeroG)
+
+        if (isZeroG)
         {
             zeroGMove();
         }
@@ -39,18 +44,25 @@ public class PlayerController : MonoBehaviour
     void zeroGMove()
     {
 
+
         Rigidbody2D rbody = GetComponent<Rigidbody2D>();
         if (rbody.velocity.magnitude > maxGlideSpd)
         {
             rbody.velocity = Vector2.ClampMagnitude(rbody.velocity, maxGlideSpd);
         }
-        rbody.velocity += new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        rbody.velocity *= Time.deltaTime * jetPackAccel;
-
-
-        if (Input.GetKey(KeyCode.E) && Input.GetKey(KeyCode.Q))
+        movementVec = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * jetPackAccel, Input.GetAxis("Vertical") * Time.deltaTime * jetPackAccel);
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            rbody.velocity = Vector2.ClampMagnitude(rbody.velocity, rbody.velocity.magnitude * brakeForce * Time.deltaTime);
+            movementVec *= overPressure;
+        }
+        rbody.AddForce(rotate2DVec(movementVec, rbody.rotation), ForceMode2D.Impulse);
+
+
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rbody.velocity = Vector2.Lerp(rbody.velocity, Vector2.zero, brakeForce * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.E))
         {
@@ -61,6 +73,16 @@ public class PlayerController : MonoBehaviour
             rbody.rotation -= rotationSpd * Time.deltaTime;
         }
 
+        velocityVec = rbody.velocity;
+
+
+    }
+    Vector2 rotate2DVec(Vector2 v, float angle)
+    {
+
+        float cos = Mathf.Cos(angle * Mathf.Deg2Rad);
+        float sin = Mathf.Sin(angle * Mathf.Deg2Rad);
+        return new Vector2(cos * v.x - sin * v.y, sin * v.x + cos * v.y);
 
     }
     void movement()
